1890ef55b2503f7214760ad3fcb49b778ed2a847
miku-death <91643510+miku-death@users.noreply.github.com>
2023-02-13 15:13:16 +0900

    updated vs-code generated file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace FinalTest
 {
@@ -6,7 +6,23 @@ namespace FinalTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("длинна массива: ");
+            int arrayLen = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[arrayLen];
+
+            for(int i = 0; i < arrayLen; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+
+            PrintShortStrings(array);
+        }
+
+        private static void PrintShortStrings(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i].Length <= 3)
+                    Console.WriteLine(array[i]);
         }
     }
 }
